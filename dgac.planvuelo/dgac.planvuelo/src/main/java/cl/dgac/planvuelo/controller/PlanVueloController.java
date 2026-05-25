c9e28e6fdf637cff6aedf3f87e5fdafdddf444ec
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2026-05-24 22:35:33 -0400

    Base sin WebClient v2

--- dgac.planvuelo/dgac.planvuelo/src/main/java/cl/dgac/planvuelo/controller/PlanVueloController.java
+++ dgac.planvuelo/dgac.planvuelo/src/main/java/cl/dgac/planvuelo/controller/PlanVueloController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,8 +48,9 @@ public class PlanVueloController {
         return ResponseEntity.status(HttpStatus.CREATED).body(pV);
     }
     
-    //@DeleteMapping
-    //public ResponseEntity<String> eliminarPV(@PathVariable int idPlanVuelo){
-        
-    //}
+    @DeleteMapping
+    public ResponseEntity<String> eliminarPV(@PathVariable int idPlanVuelo){
+        pVueloService.eliminarPlanesVuelo(idPlanVuelo);
+        return ResponseEntity.noContent().build();
+    }
 }
